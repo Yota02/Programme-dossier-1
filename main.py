d88e74ba6f21e55a77d5a906874e6aaba73edeaa
Yota002 <amichauxkinet@gmail.com>
2023-04-12 12:19:48 +0200

    programme fini

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 chemin_dossier = "C:/Users/amich/OneDrive/Images/img" # Chemin du dossier à parcourir
-liste_dossiers = [] # Initialisation de la liste des dossiers
+liste_dossiers = [nom for nom in os.listdir(chemin_dossier) if os.path.isdir(os.path.join(chemin_dossier, nom))] # Initialisation de la liste des dossiers
 
 for nom in os.listdir(chemin_dossier): # Parcours du dossier et ajout des noms des dossiers dans la liste
     if os.path.isdir(os.path.join(chemin_dossier, nom)):
@@ -10,14 +10,17 @@ for nom in os.listdir(chemin_dossier): # Parcours du dossier et ajout des noms d
 print(liste_dossiers)
 
 def rename(liste):
-    for i in range(len(liste)): #changer ce parcours 
-        chemin_dossier = "C:/Users/amich/OneDrive/Images/img" # Chemin du dossier contenant les images à renommer
-        i = 1
-        for fichier in sorted(os.listdir(chemin_dossier)): # Parcours tous les fichiers du dossier
+    for i in range(len(liste)):
+        chemin_dossier2 = "C:/Users/amich/OneDrive/Images/img/" + str(liste[i]) # Chemin du dossier contenant les images à renommer
+        print(chemin_dossier2)
+        num = 1
+        for fichier in sorted(os.listdir(chemin_dossier2)): # Parcours tous les fichiers du dossier
             if not fichier.endswith((".jpg", ".jpeg", ".png", ".gif")): # Ignore les fichiers non-images 
                 continue
-            nouveau_nom = os.path.basename(chemin_dossier) + "!" + str(i).zfill(4) + os.path.splitext(fichier)[1] # Génère un nouveau nom pour le fichier
-            os.rename(os.path.join(chemin_dossier, fichier), os.path.join(chemin_dossier, nouveau_nom)) # Renomme le fichier avec le nouveau nom
-            i += 1
+            nouveau_nom = os.path.basename(chemin_dossier2) + "!" + str(num).zfill(4) + os.path.splitext(fichier)[1] # Génère un nouveau nom pour le fichier
+            os.rename(os.path.join(chemin_dossier2, fichier), os.path.join(chemin_dossier2, nouveau_nom)) # Renomme le fichier avec le nouveau nom
+            num += 1
 
-    return nouveau_nom
+    return "Renommage terminé !"
+
+print(rename(liste_dossiers))
